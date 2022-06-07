e6fed10f22a3dd699fba774c650ba4d4c4503ac3
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2022-06-07 23:33:59 +0600

    first commit

---
+++ dayOne.py
@@ -0,0 +1,35 @@
+import turtle
+# import calendar
+# import datatime
+
+hridoy_turtle = turtle.Turtle()
+hridoy_turtle.forward(100)
+hridoy_turtle.right(100)
+hridoy_turtle.forward(100)
+hridoy_turtle.right(100)
+# hridoy_turtle.right(300)
+hridoy_turtle.forward(300)
+hridoy_turtle.left(300)
+hridoy_turtle.right(100)
+
+# a=189
+
+# print(a)
+
+# def hridoy():
+#   country = 'Bangladesh'
+#   capital = "Dhaka"
+#   district = 64
+#   area = 147570
+#   print(country)
+#   print(capital)
+#   print(district)
+#   print(area)
+    
+
+# hridoy()
+
+# print(calendar.calendar(2022))
+# print(calendar.month(2022,12))
+# print(calendar.weekday(2022,8,11))
+# print(calendar.isleap(2002))
